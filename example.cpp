ec07c245584b501cab7ddc711fdb4d1e2e1d5ebb
Naoto Uehara <nawonaototomousu@gmail.com>
2020-05-16 20:11:29 +0900

    improved

--- example.cpp
+++ example.cpp
@@ -6,17 +6,28 @@ using namespace satch;
 
 int main()
 {
-    auto v = std::variant<int,std::string,double,float>("aaa");
-    
-    auto res = Match{v}
-    (
-        Case<int>(0),[](auto&) {std::cout << "contains int value 0" << std::endl; return 0;},
-        Case<std::string>("aaa"),[](auto&) {return std::string("aaa->bbb"); },
-        Case<float>(),[](auto& va) {std::cout << "contains float " << std::get<float>(va) << std::endl; return 0; },
-        Case<double>(),[](auto&) {std::cout << "contains double" << std::endl; return 0; }
-    );
+    auto v = std::variant<int, std::string, double, float>("aaa");
 
-    std::cout << "result of matching: " << std::get<std::string>(res) << std::endl;
-    
+    auto res = Match{v}(
+        Case<int>(0),
+        [](auto&) {
+            std::cout << "contains int value 0" << std::endl;
+            return 0;
+        },
+        Case<std::string>("aaa"), [](auto&) { return std::string("aaa->bbb"); },
+        Case<float>(),
+        [](auto& va) {
+            std::cout << "contains float " << std::get<float>(va) << std::endl;
+            return 0;
+        },
+        Case<double>(),
+        [](auto&) {
+            std::cout << "contains double" << std::endl;
+            return 0;
+        });
+
+    std::cout << "result of matching: " << std::get<std::string>(res)
+              << std::endl;
+
     return 0;
 }
