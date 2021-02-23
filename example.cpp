20746f4e113bba9fc30bb5545ba7348a6354705b
Naoto Uehara <nawonaototomousu@gmail.com>
2021-02-23 19:35:41 +0900

    single return type

--- example.cpp
+++ example.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <optional>
 #include <variant>
 #include <string>
 #include "pattern_match.hpp"
@@ -6,28 +7,32 @@ using namespace satch;
 
 int main()
 {
-    auto v = std::variant<int, std::string, double, float>("aaa");
+    auto variant = std::variant<int, std::string, double, float>("ABC");
 
-    auto res = Match{v}(
-        Case<int>(0),
-        [](auto&&) {
-            std::cout << "contains int value 0" << std::endl;
-            return 0;
-        },
-        Case<std::string>("aaa"), [](auto&& str) {std::cout << str << std::endl;return 10; },
-        Case<float>(),
-        [](auto&& va) {
-            std::cout << "contains float " << va << std::endl;
-            return 0;
-        },
-        Case<double>(),
-        [](auto&&) {
-            std::cout << "contains double" << std::endl;
-            return 0;
-        });
+    auto result = Match{variant}(
+        Case<int>(0), [](auto&&) 
+            {
+                std::cout << "variant contains int value 0" << std::endl;
+                return 0;
+            },
+        Case<std::string>("ABC"), [](auto&& str) 
+            {
+                std::cout << str << std::endl;
+                return 10; 
+            },
+        Case<float>(),[](auto&& val) 
+            {
+                std::cout << "variant contains float " << va << std::endl;
+                return 0;
+            },
+        Case<double>(), [](auto&&) 
+            {
+                std::cout << "variant contains double" << std::endl;
+                return 0;
+            }
+        );
 
-    std::cout << "result of matching: " << std::get<1>(res)
-              << std::endl;
+    std::cout << "result of matching: " << result.value() << std::endl;
 
     return 0;
 }
